5a2b9f35fad4f10b6b6a12f1e3b51b465ee09344
NEKOMARU23 <nekosuki29@icloud.com>
2026-03-26 03:27:34 +0900

    修正

--- CiTest/Assets/Scripting/test.cs
+++ CiTest/Assets/Scripting/test.cs
@@ -10,7 +10,7 @@ public class InversionTest : MonoBehaviour
     void Update()
     {
         // スペースキーを押すと重力を反転させる（予定）
-        if (Input.GetKeyDown(KeyCode.Space)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             ReverseGravity();// ← あえてセミコロンを抜いています！
         }   
