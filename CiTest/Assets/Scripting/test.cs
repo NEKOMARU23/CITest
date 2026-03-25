e32cc33b9d8dc110950105c20e9820d4167facff
NEKOMARU23 <nekosuki29@icloud.com>
2026-03-25 15:44:03 +0900

    エラー通知実験

---
+++ CiTest/Assets/Scripting/test.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class InversionTest : MonoBehaviour
+{
+    void Update()
+    {
+        // スペースキーを押すと重力を反転させる（予定）
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ReverseGravity() // ← あえてセミコロンを抜いています！
+        }
+    }
+
+    void ReverseGravity()
+    {
+        Physics2D.gravity *= -1;
+        Debug.Log("Gravity Inverted: " + Physics2D.gravity);
+    }
+}
